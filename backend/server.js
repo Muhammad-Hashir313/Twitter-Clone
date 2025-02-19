a99af2f97085db34ee7e47ebae0a84072fe3e04e
Muhammad Hashir <mehashir313@gmail.com>
2025-02-19 10:54:00 +0500

    Initial Commit

---
+++ backend/server.js
@@ -0,0 +1,9 @@
+const express = require('express')
+const dotenv = require('dotenv').config()
+const colors = require('colors')
+
+const PORT = process.env.PORT || 5000
+
+const app = express()
+
+app.listen(PORT, () => console.log(`Server starting at port: ${PORT}`))
