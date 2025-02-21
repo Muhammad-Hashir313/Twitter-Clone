c351a2c6c198c19abe83ba911722fb80fe8674d1
Muhammad Hashir <mehashir313@gmail.com>
2025-02-21 16:43:41 +0500

    User Authentication Setup

--- backend/controllers/userController.js
+++ backend/controllers/userController.js
@@ -1,17 +1,69 @@
 const asyncHandler = require('express-async-handler')
+const bcrypt = require('bcryptjs')
+const User = require('../models/userModel')
 
 // @desc    Register a New User
 // @route   GET /api/users
 // @access  Public
 const registerUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Register user" })
+    const { name, email, password, password2 } = req.body
+
+    if (!name || !email || !password) {
+        res.status(400)
+        throw new Error('Please enter all fields')
+    }
+
+    let existsUser = await User.findOne({ email })
+    if (existsUser) {
+        res.status(400)
+        throw new Error('User with the email already exists')
+    }
+
+    // Hash Password
+    const salt = await bcrypt.genSalt(10)
+    const hashedPassword = await bcrypt.hash(password, salt)
+
+    // Create User
+    const user = await User.create({
+        name,
+        email,
+        password: hashedPassword
+    })
+
+    if (user) {
+        res.status(201).json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid user data')
+    }
 })
 
 // @desc    Authenticate user
 // @route   GET /api/users/login
 // @access  Public
 const loginUser = asyncHandler(async (req, res) => {
-    res.json({ message: "Login user" })
+    const { email, password } = req.body
+
+    if (!email || !password) {
+        res.status(400)
+        throw new Error('Please enter all fields')
+    }
+
+    const user = await User.findOne({ email })
+    if (user && (await bcrypt.compare(password, user.password))) {
+        res.json({
+            _id: user._id,
+            name: user.name,
+            email: user.email,
+        })
+    } else {
+        res.status(400)
+        throw new Error('Invalid Credentials')
+    }
 })
 
 // @desc    Get user data
