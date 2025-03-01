e7c91aa394f448d02830e2d931a514fbd6cf98f2
Muhammad Hashir <mehashir313@gmail.com>
2025-03-01 11:48:15 +0500

    Tweet handling done using mysql

--- backend/controllers/tweetController.js
+++ backend/controllers/tweetController.js
@@ -1,13 +1,19 @@
 const asyncHandler = require('express-async-handler')
-const Tweet = require('../models/tweetModel')
+// const Tweet = require('../models/tweetModel')
+const conn = require('../config/db')
 
 // @desc    Get Tweets
 // @route   GET /api/tweets
 // @access  Private
 const getTweets = asyncHandler(async (req, res) => {
-    const tweets = await Tweet.find({ user: req.user.id })
+    conn.query('SELECT * FROM TWEETS', (err, results) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).json({ message: 'Error getting tweets' })
+        }
 
-    res.status(200).json(tweets)
+        res.status(200).json(results)
+    })
 })
 
 // @desc    Create a Tweet
@@ -19,16 +25,21 @@ const createTweet = asyncHandler(async (req, res) => {
         throw new Error('Please add some text')
     }
 
-    const newTweet = await Tweet.create({
-        user: req.user.id,
-        text: req.body.text
-    })
+    conn.query('INSERT INTO TWEETS (TEXT) VALUES (?)', [req.body.text], (err, results) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).json({ message: 'Error creating tweet!' })
+        }
 
-    res.status(201).json(newTweet)
+        res.status(201).json({
+            id: results.insertId,
+            tweet: req.body.text
+        })
+    })
 })
 
 // @desc    Update a Tweet
-// @route   UPDATE /api/tweets/:id
+// @route   PUT /api/tweets/:id
 // @access  Private
 const updateTweet = asyncHandler(async (req, res) => {
     if (!req.body.text) {
@@ -37,52 +48,60 @@ const updateTweet = asyncHandler(async (req, res) => {
     }
 
     const id = req.params.id
-    if (!req.user) {
-        res.status(401)
-        throw new Error('User not found')
-    }
+    const text = req.body.text
 
-    const tweet = await Tweet.findById(id)
-    if (!tweet) {
-        res.status(400)
-        throw new Error('Tweet not found!')
-    }
+    conn.query('SELECT * FROM TWEETS WHERE ID = ?', [id], (err, results) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).json({ message: 'Error checking tweet!' })
+        }
 
-    if (tweet.user.toString() !== req.user.id) {
-        res.status(401)
-        throw new Error('Not Authorized')
-    }
+        if (results.length === 0) {
+            return res.status(404).json({ message: 'Tweet not found!' })
+        }
 
-    const updatedTweet = await Tweet.findByIdAndUpdate(id, req.body, { new: true })
 
-    res.status(200).json(updatedTweet)
+        conn.query('UPDATE TWEETS SET TEXT = ? WHERE ID = ?', [text, id], (err) => {
+            if (err) {
+                console.error(err)
+                return res.status(500).json({ message: 'Error updating tweets' })
+            }
+
+            res.status(200).json({
+                id: id,
+                text: text
+            })
+        })
+    })
 })
 
 // @desc    Delete a Tweet
-// @route   GET /api/tweets/:id
+// @route   DELETE /api/tweets/:id
 // @access  Private
 const deleteTweet = asyncHandler(async (req, res) => {
     const id = req.params.id
 
-    if (!req.user) {
-        res.status(401)
-        throw new Error('User not found')
-    }
+    // If tweet exists
+    conn.query('SELECT * FROM TWEETS WHERE ID = ?', [id], (err, results) => {
+        if (err) {
+            console.error(err)
+            return res.status(500).json({ message: 'Error checking tweet!' })
+        }
 
-    const tweet = await Tweet.findById(id)
-    if (!tweet) {
-        res.status(400)
-        throw new Error('Tweet not found!')
-    }
+        if (results.length === 0) {
+            return res.status(404).json({ message: 'Tweet not found!' })
+        }
 
-    if (tweet.user.toString() !== req.user.id) {
-        res.status(401)
-        throw new Error('Not Authorized')
-    }
 
-    await Tweet.findByIdAndDelete(id)
+        conn.query('DELETE FROM TWEETS WHERE ID = ?', [id], (err) => {
+            if (err) {
+                console.error(err)
+                return res.status(500).json({ message: 'Error deleting tweet!' })
+            }
 
-    res.status(200).json({ id: id })
+            res.status(200).json({ id: id })
+        })
+    })
 })
 
 module.exports = {
--- backend/routes/tweetRoute.js
+++ backend/routes/tweetRoute.js
@@ -1,9 +1,9 @@
 const express = require('express')
 const router = express.Router()
-const protect = require('../middleware/authMiddleware')
+// const protect = require('../middleware/authMiddleware')
 const { getTweets, createTweet, updateTweet, deleteTweet } = require('../controllers/tweetController')
 
-router.route('/').get(protect, getTweets).post(protect, createTweet)
-router.route('/:id').put(protect, updateTweet).delete(protect, deleteTweet)
+router.route('/').get(getTweets).post(createTweet)
+router.route('/:id').put(updateTweet).delete(deleteTweet)
 
 module.exports = router
